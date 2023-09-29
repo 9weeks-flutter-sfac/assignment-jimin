98f5ce878531bf64bdbb16567d346ffa419b2212
cmkoi1 <cmkoi1003@gmail.com>
2023-09-30 00:33:14 +0900

    4일차 2번 과제 완료

--- day4/assignment1/lib/main.dart
+++ day4/assignment1/lib/main.dart
@@ -6,120 +6,154 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: Calculator(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Calculator extends StatelessWidget {
+  const Calculator({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  showResultDialog(BuildContext context, var result) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width / 2,
+            height: 150,
+            child: Center(
+                child: Text(
+              "$result",
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            )),
+          ),
+        );
+      },
+    );
   }
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    TextEditingController textEditingController1 = TextEditingController();
+    TextEditingController textEditingController2 = TextEditingController();
+
+    int add(int value1, int value2) {
+      return value1 + value2;
+    }
+
+    int multiple(int value1, int value2) {
+      return value1 * value2;
+    }
+
+    int minus(int value1, int value2) {
+      return value1 - value2;
+    }
+
+    double divide(double value1, double value2) {
+      return value1 / value2;
+    }
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('사칙연산'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Row(
+              children: [
+                const Text('X의 값은?'),
+                SizedBox(
+                  width: 220,
+                  height: 50,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 48),
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      controller: textEditingController1,
+                      decoration: const InputDecoration(
+                          hintText: 'x값을 입력하세요.',
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.blue)),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey))),
+                    ),
+                  ),
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Row(
+              children: [
+                const Text('Y의 값은?'),
+                SizedBox(
+                  width: 220,
+                  height: 50,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 48),
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      controller: textEditingController2,
+                      decoration: const InputDecoration(
+                          hintText: 'y값을 입력하세요.',
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.blue)),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey))),
+                    ),
+                  ),
+                )
+              ],
             ),
+            ElevatedButton(
+                onPressed: () {
+                  showResultDialog(
+                      context,
+                      add(int.parse(textEditingController1.text),
+                          int.parse(textEditingController2.text)));
+                },
+                child: const Text('더하기의 결과값은?!')),
+            ElevatedButton(
+                onPressed: () {
+                  showResultDialog(
+                      context,
+                      multiple(
+                        int.parse(textEditingController1.text),
+                        int.parse(textEditingController2.text),
+                      ));
+                },
+                child: const Text('곱하기의 결과값은?!')),
+            ElevatedButton(
+                onPressed: () {
+                  showResultDialog(
+                      context,
+                      minus(
+                        int.parse(textEditingController1.text),
+                        int.parse(textEditingController2.text),
+                      ));
+                },
+                child: const Text('빼기의 결과값은?!')),
+            ElevatedButton(
+                onPressed: () {
+                  showResultDialog(
+                      context,
+                      divide(
+                        double.parse(textEditingController1.text),
+                        double.parse(textEditingController2.text),
+                      ));
+                },
+                child: const Text('나누기의 결과값은?!'))
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
